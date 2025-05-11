d3a3ddf6bc65fa050c8aabbbcc7b7a0949d8fd7e
mohamed abdelrahman <131889931+m7amh@users.noreply.github.com>
2025-05-11 20:29:34 +0300

    Update log_analyzer.sh

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -70,6 +70,22 @@ grep -a '"GET ' "$LOG_FILE" | awk '/\[.*\] "[A-Z]+ .* HTTP\// {print $1}' | sort
 echo "Most Active IP by POST:"
 grep -a '"POST ' "$LOG_FILE" | awk '/\[.*\] "[A-Z]+ .* HTTP\// {print $1}' | sort | uniq -c | sort -nr | head -1 | awk '{print $2, "(" $1 " POSTs)"}'
 
+# 10.1 Top 4 IPs making GET requests
+echo -e "\nTop 4 IPs with most GET requests:"
+grep -a '"GET ' "$LOG_FILE" | awk '{print $1}' | sort | uniq -c | sort -nr | head -4 | awk '{print $2, "(" $1 " GETs)"}'
+
+# 10.2 Top 4 requested URLs using GET
+echo -e "\nTop 4 URLs requested via GET:"
+grep -a '"GET ' "$LOG_FILE" | awk -F\" '{print $2}' | awk '{print $2}' | sort | uniq -c | sort -nr | head -4 | awk '{print $2, "(" $1 " times)"}'
+
+# 10.3 Top 4 IPs making POST requests
+echo -e "\nTop 4 IPs with most POST requests:"
+grep -a '"POST ' "$LOG_FILE" | awk '{print $1}' | sort | uniq -c | sort -nr | head -4 | awk '{print $2, "(" $1 " POSTs)"}'
+
+# 10.4 Top 4 requested URLs using POST
+echo -e "\nTop 4 URLs requested via POST:"
+grep -a '"POST ' "$LOG_FILE" | awk -F\" '{print $2}' | awk '{print $2}' | sort | uniq -c | sort -nr | head -4 | awk '{print $2, "(" $1 " times)"}'
+
 # 11. Failure Patterns by Hour
 echo -e "\nFailure Requests by Hour:"
 awk '/\[.*\] "[A-Z]+ .* HTTP\// && $9 ~ /^[45][0-9][0-9]/ {match($0, /\[.*:([0-9]{2}):/, a); print a[1]}' "$LOG_FILE" | sort | uniq -c | sort -nr | awk '{print "Hour", $2, ":", $1}'
